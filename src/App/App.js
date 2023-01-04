5519db07467ac04015bc4a72fa57f8b560080e24
Alexy <alexyddb@gmail.com>
2023-01-04 14:34:15 +0100

    init

---
+++ src/App/App.js
@@ -0,0 +1,13 @@
+import styles from "./App.module.scss";
+
+function App() {
+  return (
+    <>
+      <div className={styles["page"]}>
+        <h1 className={styles["pageTitle"]}>Le livre de livres</h1>
+      </div>
+    </>
+  );
+}
+
+export default App;
