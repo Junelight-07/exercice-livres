96e2c257e6a2eb72729307322091cfbbf1f4393c
Alexy <alexyddb@gmail.com>
2023-02-28 11:00:57 +0100

    Revert "pour supprimer"
    This reverts commit a0a93ef350645be56c0661b471df6e97f384c9b9.

--- src/App/pages/books/components/Favorites/Favorites.test.js
+++
@@ -1,8 +0,0 @@
-import { render, screen } from "@testing-library/react";
-import Favorites from "./Favorites";
-
-test("renders learn react link", () => {
-  render(<Favorites />);
-  const linkElement = screen.getByText(/Vos favoris/i);
-  expect(linkElement).toBeInTheDocument();
-});
