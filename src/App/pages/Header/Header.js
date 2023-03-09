a5d124abd88b3f6a292e778075fc8abb2a9b14e3
Alexy <alexyddb@gmail.com>
2023-03-09 09:51:29 +0100

    V13.2 finie avec deplacement des images dans public

--- src/App/pages/Header/Header.js
+++ src/App/pages/Header/Header.js
@@ -1,11 +1,11 @@
 import styles from "./Header.module.scss";
-import logo from "../../../assets/livresLogo.jpg";
+import React from "react";
 
 export default function Header() {
   return (
     <header className={styles["pageTitle"]}>
       <h1>Le livre de livres</h1>
-      <img src={logo} alt="logo de livres" />
+      <img src={"./livresLogo.jpg"} alt="logo de livres" />
     </header>
   );
 }
